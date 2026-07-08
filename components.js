1fbca33672f45708dece3ec2fde0b5678a265ffb
BasedLukas <BasedLukas@users.noreply.github.com>
2026-07-08 21:23:23 +0000

    deploy: e8ec3ae70667303a16d5693fdfffd8ae1ab27d67

--- components.js
+++ components.js
@@ -1,7 +1,7 @@
 (function () {
   var src = document.currentScript.getAttribute('src');
-  var base = src.replace('components.js', '');
-  var isHome = base === '' || base === './';
+  var isHome = src === 'components.js' || src === './components.js';
+  var root = '/';
 
   // Umami analytics
   var s = document.createElement('script');
@@ -15,14 +15,14 @@
   if (nav) {
     if (isHome) {
       nav.innerHTML =
-        '<a href="' + base + 'blog/">Blog</a>' +
-        '<a href="' + base + 'links/">Links</a>';
+        '<a href="' + root + 'projects/">Projects</a>' +
+        '<a href="' + root + 'links/">Links</a>';
     } else {
       nav.innerHTML =
-        '<a class="nav-home" href="' + base + '">Loreley</a>' +
+        '<a class="nav-home" href="' + root + '">Loreley</a>' +
         '<div class="nav-links">' +
-          '<a href="' + base + 'blog/">Blog</a>' +
-          '<a href="' + base + 'links/">Links</a>' +
+          '<a href="' + root + 'projects/">Projects</a>' +
+          '<a href="' + root + 'links/">Links</a>' +
         '</div>';
     }
   }
@@ -36,7 +36,7 @@
         '<a href="mailto:info@loreley.one">Email</a>' +
         '<a href="https://www.linkedin.com/in/lukas-bogacz/">LinkedIn</a>' +
         '<a href="https://github.com/basedlukas">GitHub</a>' +
-        '<a href="' + base + 'blog/">Blog</a>' +
+        '<a href="' + root + 'projects/">Projects</a>' +
       '</div>';
   }
 })();
